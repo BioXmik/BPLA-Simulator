7fe516a9dfc1ddcf7c2c548f82b721b03e588f11
BioXmik <77557979+BioXmik@users.noreply.github.com>
2023-05-29 02:52:25 +0300

    fgdgfddgf

--- Assets/Scripts/drop.cs
+++ Assets/Scripts/drop.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class drop : MonoBehaviour
 {
+	public bool cargoIsDroped;
+	public GameObject cargo;
 	public GameObject dropCargo;
 	
 	public void DropCargo()
 	{
 		if (!GetComponent<Sensors>().killSwitch && GetComponent<battery>().energy > 0)
 		{
-			GameObject newDrop = Instantiate(dropCargo, new Vector3(transform.position.x, transform.position.y -2f, transform.position.z), Quaternion.identity);
-			newDrop.GetComponent<Rigidbody>().mass = PlayerPrefs.GetFloat("DropMass");
+			if (!cargoIsDroped)
+			{
+				cargoIsDroped = true;
+				cargo.SetActive(false);
+				GameObject newDrop = Instantiate(dropCargo, new Vector3(transform.position.x, transform.position.y -1f, transform.position.z), Quaternion.identity);
+				newDrop.GetComponent<Rigidbody>().mass = PlayerPrefs.GetFloat("DropMass");
+			}
 		}
 	}
 }
